821a1f2f386856af28eb1fe2d5a8c99f90c72a05
Abhishek Shamra <abhishek.er45@gmail.com>
2025-09-21 18:15:49 +0530

    change text in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer 
     className="py-8 bg-dark-300">
         <div className="container mx-auto px-6 text-center">
-            <p className="text-gray-400">© 2023 Developer Portfolio. All rights reserved.</p>
+            <p className="text-gray-400">© 2025 Created By TheAbhishekCodes. All rights reserved.</p>
         </div>
     </footer>
   )
